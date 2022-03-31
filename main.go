aed48e8ce0925bc22bee6e6a92959ba81a23550a
xu.jin <xyjy247@gmail.com>
2022-03-31 18:09:28 +0800

    review

--- main.go
+++ main.go
@@ -21,9 +21,8 @@ func main() {
 
 	r := routers.SetupRouter()
 	//3.监听端口，默认在8080
-	//Run("里面不指定端口号默认为8080")
 	if err := r.Run(":" + Port); err != nil {
-		fmt.Println("startup service failed, err:%v\n", err)
+		fmt.Printf("startup service failed, err:%v\n\n", err)
 	}
 
 }
