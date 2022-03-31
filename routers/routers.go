895e885ad4b5fa1be88356f741bdb03debbd170c
xu.jin <xyjy247@gmail.com>
2022-03-31 16:52:51 +0800

    fix:User

--- routers/routers.go
+++ routers/routers.go
@@ -1,11 +1,15 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"Gin_web/apps/users"
+	"github.com/gin-gonic/gin"
+)
 
 // SetupRouter 配置路由信息
 func SetupRouter() *gin.Engine {
 	// 创建路由
 	r := gin.Default()
 	LoadV1(r)
+	users.LoadUserRouter(r)
 	return r
 }
