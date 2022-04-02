67c528fe61cfc2ae2891b8e548295fe0cc81d5d4
xu.jin <xyjy247@gmail.com>
2022-04-02 18:10:27 +0800

    用户注册

--- apps/users/handler.go
+++ apps/users/handler.go
@@ -15,14 +15,29 @@ func FindUsers(c *gin.Context) {
 
 }
 
+type User struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
 // CreateUser 注册用户
 func CreateUser(c *gin.Context) {
-	username := c.PostForm("username")
-	password := c.PostForm("password")
-	user := models.User{Username: username, Password: password}
-	models.DB.Create(user)
-	//fmt.Println(result.Error)
-	c.String(http.StatusOK, "创建用户%s成功", user.Username)
+	var user User
+	if c.BindJSON(&user) == nil {
+		exist := models.DB.Where("username = ?", user.Username).First(&models.User{}).Row()
+		if exist == nil {
+			user := models.User{Username: user.Username, Password: user.Password}
+			result := models.DB.Create(&user)
+			if result.Error != nil {
+				c.JSON(200, gin.H{"status": "失败"})
+			} else {
+				c.JSON(200, gin.H{"status": "芜湖湖"})
+			}
+		} else {
+			c.JSON(200, gin.H{"status": "用户名已存在"})
+		}
+	}
+
 }
 
 //todo 写入问题 db
