885dd97a7602643ea1da339c54f1ec182679de4b
xu.jin <xyjy247@gmail.com>
2022-04-14 17:22:48 +0800

    路由分组

--- internal/service/hosts/router.go
+++ internal/service/hosts/router.go
@@ -3,10 +3,12 @@ package hosts
 import "github.com/gin-gonic/gin"
 
 func LoadHostRouter(r *gin.Engine) {
-	r.GET("hosts/:hostId", FindHost)
-	r.GET("hosts", FindHosts)
-	r.POST("hosts", CreateHost)
-	r.DELETE("hosts/:hostId", DeleteHost)
-	r.DELETE("hosts", DeleteHosts)
-	r.PATCH("hosts/:hostId", UpdateHost)
+	HostsRouter := r.Group("/hosts")
+
+	HostsRouter.GET("/:hostId", FindHost)
+	HostsRouter.GET("", FindHosts)
+	HostsRouter.POST("", CreateHost)
+	HostsRouter.DELETE("/:hostId", DeleteHost)
+	HostsRouter.DELETE("", DeleteHosts)
+	HostsRouter.PATCH("/:hostId", UpdateHost)
 }
